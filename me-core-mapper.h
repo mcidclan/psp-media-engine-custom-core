f056d4f1107de35935da677a2633310511fcc2e2
m-c/d <534348+mcidclan@users.noreply.github.com>
2025-08-22 18:20:13 +0200

    add hwMutex functions and storageBusClock functions for slim

--- me-core-mapper.h
+++ me-core-mapper.h
@@ -10,20 +10,30 @@
 #define ME_CORE_BL_IMG_TABLE                2
 #define ME_CORE_IMG_TABLE                   3
 
+#define ME_CORE_BASE_ADDR                   0x88300000
 #define ME_CORE_KERNEL_ADDR                 0x88300000
 
 #define sysCall(index)                      ((void*)systemTable[(index)])
 
 // me core functions
-#define meCoreDcacheWritebackInvalidateAll  ((void(*)(void))sysCall(_meCoreDcacheWritebackInvalidateAll))
-#define meCoreBusClockEnable                ((int(*)(u32))sysCall(_meCoreBusClockEnable))
-#define meCoreBusClockFilterEnabled         ((int(*)(u32))sysCall(_meCoreBusClockFilterEnabled))
+#define meCoreDcacheWritebackInvalidateAll    ((void(*)(void))sysCall(_meCoreDcacheWritebackInvalidateAll))
+// ..
+#define meCoreBusClockEnable                  ((int(*)(u32))sysCall(_meCoreBusClockEnable))
+#define meCoreBusClockFilterEnabled           ((int(*)(u32))sysCall(_meCoreBusClockFilterEnabled))
+#define meCoreUnlockHwMutex                   ((int(*)(void))sysCall(_meCoreUnlockHwMutex))
+#define meCoresceKernelTryLockHwMutex         ((int(*)(void))sysCall(_meCoreTryLockHwMutex))
+#define meCoreSelectMemoryStickBusClock       ((int(*)(u32))sysCall(_meCoreSelectMemoryStickBusClock))
+#define meCoreEnableDisableStorageBusClock    ((int(*)(u32, u32))sysCall(_meCoreEnableDisableStorageBusClock))
 
 
 enum {
   _meCoreDcacheWritebackInvalidateAll,
   _meCoreBusClockEnable,
   _meCoreBusClockFilterEnabled,
+  _meCoreUnlockHwMutex,
+  _meCoreTryLockHwMutex,
+  _meCoreSelectMemoryStickBusClock,
+  _meCoreEnableDisableStorageBusClock,
   count
 };
 
@@ -31,30 +41,34 @@ enum {
 
 // img
 const u32 meImgTable[count] __attribute__((aligned(64))) = {
-  (ME_CORE_KERNEL_ADDR | 0x0007b3bc), // _meCoreDcacheWritebackInvalidateAll
-  (ME_CORE_KERNEL_ADDR | 0x00001970), // _meCoreBusClockEnable
-  (ME_CORE_KERNEL_ADDR | 0x000019c0), // _meCoreBusClockFilterEnabled
+  (ME_CORE_KERNEL_ADDR | 0x0007b3bc), // _meCoreDcacheWritebackInvalidateAll(void)
+  (ME_CORE_KERNEL_ADDR | 0x00001970), // _meCoreBusClockEnable(u32)
+  (ME_CORE_KERNEL_ADDR | 0x000019c0), // _meCoreBusClockFilterEnabled(u32)
 };
 
 // bl
 const u32 blImgTable[count] __attribute__((aligned(64))) = {
-  (ME_CORE_KERNEL_ADDR | 0x00031398), // _meCoreDcacheWritebackInvalidateAll
-  (ME_CORE_KERNEL_ADDR | 0x00001520), // _meCoreBusClockEnable
-  (ME_CORE_KERNEL_ADDR | 0x00001570), // _meCoreBusClockFilterEnabled
+  (ME_CORE_KERNEL_ADDR | 0x00031398), // _meCoreDcacheWritebackInvalidateAll(void)
+  (ME_CORE_KERNEL_ADDR | 0x00001520), // _meCoreBusClockEnable(u32)
+  (ME_CORE_KERNEL_ADDR | 0x00001570), // _meCoreBusClockFilterEnabled(u32)
 };
 
 // sd
 const u32 sdImgTable[count] __attribute__((aligned(64))) = {
-  (ME_CORE_KERNEL_ADDR | 0x0002ddd0), // _meCoreDcacheWritebackInvalidateAll
-  (ME_CORE_KERNEL_ADDR | 0x00001660), // _meCoreBusClockEnable
-  (ME_CORE_KERNEL_ADDR | 0x000016b0), // _meCoreBusClockFilterEnabled
+  (ME_CORE_KERNEL_ADDR | 0x0002ddd0), // _meCoreDcacheWritebackInvalidateAll(void)
+  (ME_CORE_KERNEL_ADDR | 0x00001660), // _meCoreBusClockEnable(u32)
+  (ME_CORE_KERNEL_ADDR | 0x000016b0), // _meCoreBusClockFilterEnabled(u32)
 };
 
 // t2img (slim and slim+ ?)
 const u32 t2ImgTable[count] __attribute__((aligned(64))) = {
-  (ME_CORE_KERNEL_ADDR | 0x0008bc10), // _meCoreDcacheWritebackInvalidateAll
-  (ME_CORE_KERNEL_ADDR | 0x000019a4), // _meCoreBusClockEnable
-  (ME_CORE_KERNEL_ADDR | 0x000019f4), // _meCoreBusClockFilterEnabled
+  (ME_CORE_KERNEL_ADDR | 0x0008bc10), // _meCoreDcacheWritebackInvalidateAll(void)
+  (ME_CORE_KERNEL_ADDR | 0x000019a4), // _meCoreBusClockEnable(u32)
+  (ME_CORE_KERNEL_ADDR | 0x000019f4), // _meCoreBusClockFilterEnabled(u32)
+  (ME_CORE_KERNEL_ADDR | 0x00021c04), // _meCoreUnlockHwMutex(void)
+  (ME_CORE_KERNEL_ADDR | 0x00021bd8), // _meCoreTryLockHwMutex(void)
+  (ME_CORE_KERNEL_ADDR | 0x00001b14), // _meCoreSelectMemoryStickBusClock(u32)
+  (ME_CORE_KERNEL_ADDR | 0x00001ab8), // _meCoreEnableDisableStorageBusClock(u32, u32) // mask, value
 };
 
 u32* systemTable = (u32*)t2ImgTable;
