9596f921cc0a87a508c47ebdf98a08d6c0d8e370
m-c/d <534348+mcidclan@users.noreply.github.com>
2025-09-06 20:43:54 +0200

    add meLibUnlockHwUserRegisters, meLibUnlockMemory, and meLibSetMinimalVmeConfig config functions to meLibHandler

--- me-lib.h
+++ me-lib.h
@@ -23,6 +23,31 @@ static inline void meLibHalt() {
   asm volatile(".word 0x70000000");
 }
 
+static inline void meLibUnlockHwUserRegisters() {
+  const u32 START = 0xbc000030;
+  const u32 END   = 0xbc000044;
+  for(u32 reg = START; reg <= END; reg+=4) {
+    hw(reg) = -1;
+  }
+  meLibSync();
+}
+
+static inline void meLibUnlockMemory() {
+  const u32 START = 0xbc000000;
+  const u32 END   = 0xbc00002c;
+  for(u32 reg = START; reg <= END; reg+=4) {
+    hw(reg) = -1;
+  }
+  meLibSync();
+}
+
+static inline void meLibSetMinimalVmeConfig() {
+  hw(0xBCC00000) = -1;
+  hw(0xBCC00030) = 1;
+  hw(0xBCC00040) = 1;
+  meLibSync();
+}
+
 static inline void meLibGetUncached32(volatile u32** const mem, const u32 size) {
   static void* _base = NULL;
   if (!_base) {
@@ -56,6 +81,10 @@ void meLibHandler() {
   HW_SYS_NMI_FLAGS             = 0xffffffff;
   meLibSync();
     
+  meLibUnlockHwUserRegisters();
+  meLibUnlockMemory();
+  meLibSetMinimalVmeConfig();
+  
   asm volatile(
     ".set noreorder                  \n"
     "li             $k0, 0x30000000  \n"
