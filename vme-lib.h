a3395295b010a243bcf04c492eb593bee32c5b30
m-c/d <534348+mcidclan@users.noreply.github.com>
2026-08-24 11:55:29 +0200

    (vme-lib): add some missing parentheses to the VME macro

--- vme-lib.h
+++ vme-lib.h
@@ -36,7 +36,7 @@
 
 
 #define VME_DEF_MODE              0x84000000
-#define agu_mode(mode)           (0x80000000 | (mode << 24))
+#define agu_mode(mode)           (0x80000000 | ((mode) << 24))
 
 #define VME_DEF_STEP              0x00010000
 #define VME_DEF_MAPPER            0x00003210
@@ -48,8 +48,9 @@
 #define VME_CYCLE_9               0x00090000
 #define VME_CYCLE_12              0x000c0000
 
-#define vme_cyc(n) (n << 16)
-
+#define vme_cyc(n) ((n) << 16)
+#define agu_step(n) ((n) << 16)
+ 
 // co-operand data source selectors
 #define VME_MUX_FRONT_NONE        0x00000000
 
